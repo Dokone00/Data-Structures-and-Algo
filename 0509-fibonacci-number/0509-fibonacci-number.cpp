33bc830501e8e60177c8f3818ca91304b8d3a624
Simarjeet Singh <89965444+Dokone00@users.noreply.github.com>
2023-10-24 17:25:16 +0530

    Time: 12 ms (37.63%), Space: 6.2 MB (48.21%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0){
-            return 0;
+        if(n<=1){
+            return n;
         }
-        if(n==1 || n==2 ){
-            return 1;
-        }
-        int p=1,c=1;
-        for(int i=3;i<=n;i++){
-            int temp = c;
-            c = p+c;
-            p = temp;
-        }
-        return c;
-        
+        return fib(n-1) + fib(n-2);
     }
 };
